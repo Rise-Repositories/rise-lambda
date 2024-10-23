de97f5f6d2bd883a02ace24ea7d2ee969831a793
AndreyR05 <andrey.rodrigues@sptech.school>
2024-10-22 21:35:46 -0300

    fix: base64

--- src/integration/helper/s3-helper.ts
+++ src/integration/helper/s3-helper.ts
@@ -5,10 +5,14 @@ class S3Helper{
     constructor(private s3Client = new S3()) {}
 
     async uploadFile(fileName: string, file: any): Promise<string> {
+        const base64Data =  Buffer.from(file.replace(/^data:\w+\/[a-zA-Z+\-.]+;base64,/, ''), 'base64');
+        const type = file.split(';')[0].split('/')[1];
+
         const params = {
             Bucket: PROPERTIES.BUCKET_NAME,
-            Key: fileName,
-            Body: file
+            Key: `${fileName}.${type}`,
+            Body: base64Data,
+            ContentEncoding: 'base64'
         };
         return new Promise((resolve, reject) => {
             this.s3Client.putObject(params, (err: any, data: any) => {
